8778264fa946c5ef0f7bc901b5be56df2ff6d43d
Ed Way <emtek995@gmail.com>
2021-10-24 14:57:22 -0500

    Simplify and return to one macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,11 @@
 #[macro_export]
 macro_rules! winstr {
     ($s:expr) => {{
-        $s.encode_utf16()
+        use std::os::windows::ffi::OsStrExt;
+        std::ffi::OsStr::new($s)
+            .encode_wide()
             .chain(Some(0).into_iter())
             .collect::<Vec<u16>>()
             .as_ptr()
     }};
-}
-
-pub trait WinStr {
-    fn to_winstr(self) -> Vec<u16>;
-}
-
-impl WinStr for &str {
-    fn to_winstr(self) -> Vec<u16> {
-        use std::ffi::OsStr;
-        use std::os::windows::ffi::OsStrExt;
-
-        OsStr::new(self)
-            .encode_wide()
-            .chain(Some(0).into_iter())
-            .collect()
-    }
 }
